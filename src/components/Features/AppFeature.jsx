d2c63e80821a347605272992b5b47c5274596f2a
Snezana1412 <snezana.lalatovic@dci-student.org>
2024-06-13 19:25:00 +0200

    Finished design of App features section

--- src/components/Features/AppFeature.jsx
+++ src/components/Features/AppFeature.jsx
@@ -1,18 +1,15 @@
-function AppFeature() {
+import AppFeatImg from "../../assets/images/app-feature-1.png";
+
+function AppFeature({ feature }) {
+  console.log(AppFeatImg);
   return (
     <div className='single-feature media'>
       <div className='feature-icon media-left'>
-        <img
-          src='https://healthlyapp.com/wp-content/themes/healthly-app/img/icons/app-feature-1.png'
-          alt=''
-        />
+        <img src={`src/assets/images/${feature.img}`} alt='' />
       </div>
       <div className='feature-details media-body'>
-        <h5 className='text-uppercase'>USER-FRIENDLY</h5>
-        <p>
-          With a simple interface, you can easily set up medication reminders or
-          schedule appointments with physicians/pharmacists.
-        </p>
+        <h5 className='text-uppercase'>{feature.title}</h5>
+        <p>{feature.description}</p>
       </div>
     </div>
   );
